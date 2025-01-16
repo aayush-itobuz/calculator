70356a6f9654de3c5e29876e9143a3bacb038d22
Aayush Kumar Gupta <aayush@itobuz.com>
2025-01-16 18:58:56 +0530

    work in progress

--- app.js
+++ app.js
@@ -1,56 +1,101 @@
-const number = document.querySelectorAll(".num");
-const output = document.querySelector(".output");
-const operator = document.querySelectorAll(".operator");
+const number = document.querySelectorAll('.num');
+const output = document.querySelector('.output');
+const operator = document.querySelectorAll('.operator');
 const equalSign = document.querySelector('.equalSign');
+const dot = document.querySelector('.dot')
+const allClear = document.querySelector('.allClear');
+const clear = document.querySelector('.clear');
+const signChange = document.querySelector('.signChange')
 
 let prevNum = '';
-let currNum = '';
+let currNum = '0';
 let calOperator = '';
 
 const display = (number) => {
   output.innerHTML = number;
 }
 
+const clearAll = () => {
+  prevNum = '';
+  currNum = '';
+  calOperator = '';
+}
+
 const inputNum = (number) => {
-  currNum += number;
+  if(currNum === '0'){
+    currNum = number;
+  }else {
+    currNum += number;
+  }
+  console.log("op " + calOperator, "prev " + prevNum, "curr " +currNum);
+}
+
+const inputDot = (dot) => {
+  currNum = String(currNum);
+  if(currNum.includes('.')){
+    return;
+  }
+  currNum += dot;
+}
+
+const changeSign = () => {
+  currNum *= -1;
+}
+
+const clearNum = () => {
+  if((currNum == "Infinity") || (currNum == "-Infinity") || (currNum == "NaN")){
+    clearAll();
+  }
+  currNum = String(currNum);
+  currNum = currNum.substring(0,currNum.length-1);
+  display(currNum);
 }
 
 const inputOperator = (operator) => {
-  prevNum = currNum;
+  console.log("op " + calOperator, "prev " + prevNum, "curr " +currNum);
+  if(currNum != ''){
+    prevNum = currNum;
+  }else if(currNum === 0){
+    prevNum = 0;
+  }
   calOperator = operator;
   currNum = '';
+  console.log("op " + calOperator, "prev " + prevNum, "curr " +currNum);
 }
 
 const calculate = () => {
   let res = '';
-  prevNum = parseInt(prevNum);
-  currNum = parseInt(currNum);
-  // console.log(currNum);
+  console.log("op " + calOperator, "prev " + prevNum, "curr " +currNum);
+  prevNum = parseFloat(prevNum);
+  currNum = parseFloat(currNum);
   
   switch(calOperator) {
     case '+':
-      console.log(currNum,prevNum);
       res = prevNum + currNum;
-      return res;
       break;
     case '-':
       res = prevNum - currNum;
       break;
-    case 'x':
+    case 'X':
       res = prevNum * currNum;
       break;
     case '/':
       res = prevNum / currNum;
       break;
     case '%':
-    res = prevNum % currNum;
-    break;
+      if(String(currNum) === ''){
+        res = prevNum / 100;
+      }else {
+        res = prevNum % currNum;
+      }
+      break;
     default:
-      return NaN;
+      return;
   }
 
   currNum = res;
   calOperator = '';
+  console.log("op " + calOperator, "prev " + prevNum, "curr " +currNum);
 }
 
 number.forEach( (num) => {
@@ -63,16 +108,35 @@ number.forEach( (num) => {
 
 operator.forEach( (operator) => {
   operator.addEventListener('click', () => {
-    console.log(operator.innerHTML);
     inputOperator(operator.innerHTML);
     display(operator.innerHTML);
   })
 })
 
 equalSign.addEventListener('click', () => {
-  console.log("equal");
   calculate();
   display(currNum);
+  console.log(currNum);
+})
+
+dot.addEventListener('click', () => {
+  inputDot(dot.innerHTML);
+  display(currNum);
+} )
+
+allClear.addEventListener('click', () => {
+  clearAll();
+  display(currNum);
+})
+
+signChange.addEventListener('click', () => {
+  changeSign();
+  display(currNum);
+})
+
+clear.addEventListener('click', () => {
+  console.log("clear");
+  clearNum();
 })
 
 
